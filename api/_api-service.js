5d97516ba8c70d95e557a1962b37350d7b21be8a
FlashBlaze <neerajlagwankar@gmail.com>
2020-03-25 12:46:34 +0530

    Added prettier

--- api/_api-service.js
+++ api/_api-service.js
@@ -7,33 +7,34 @@ module.exports = {
     try {
       const key = process.env.goodreads_key;
       const goodreadsResponseXML = await got(
-          `https://www.goodreads.com/review/list/92378107.xml?key=${key}&v=2&shelf=read`,
+        `https://www.goodreads.com/review/list/92378107.xml?key=${key}&v=2&shelf=read`
       );
 
       goodreadsResponseJSON = convert.xml2json(goodreadsResponseXML.body, {
         compact: true
       });
 
-      const reviews = JSON.parse(goodreadsResponseJSON).GoodreadsResponse.reviews.review;
+      const reviews = JSON.parse(goodreadsResponseJSON).GoodreadsResponse
+        .reviews.review;
 
       const bookDetails = reviews.map((review) => {
         return {
           book: {
             title: review.book.title._text,
-            isbn: review.book.isbn13._text,
+            isbn: review.book.isbn13._text
           },
           review: {
             body: striptags(review.body._cdata),
             url: review.url._text,
-            rating: review.rating._text,
-          },
+            rating: review.rating._text
+          }
         };
       });
       return bookDetails;
     } catch (error) {
       return {
-        error: error.message,
+        error: error.message
       };
     }
-  },
+  }
 };
