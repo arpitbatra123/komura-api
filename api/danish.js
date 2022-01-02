df09cf5f3c4a5faafa2acdad65046f93c85e7b0f
Arpit Batra <arpitbatra123@gmail.com>
2022-01-02 20:15:43 +0530

    per page 200

--- api/danish.js
+++ api/danish.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { getReviews } = require('./_api-service');
 
 module.exports = async (req, res) => {
-  const feedUrl = `https://www.goodreads.com/review/list_rss/68951276?shelf=read`;
+  const feedUrl = `https://www.goodreads.com/review/list_rss/68951276?shelf=read&per_page=200`;
   const reviewData = await getReviews(feedUrl);
   return res.json({
     data: reviewData
